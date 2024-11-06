26e42a939a022836a3f142f11267f0fd4c938d4b
silvercen <abelkm123@gmail.com>
2024-11-06 15:35:36 +0530

    Created roles-service

---
+++ src/main/java/com/example/roles_service/RolesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.roles_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolesServiceApplication.class, args);
+	}
+
+}
